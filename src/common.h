6298b5e5a850710fdd0ac90570929e52b7591fc6
Rafael Nakano <lazpeng@gmail.com>
2025-10-03 01:32:26 -0300

    Bump the version number

--- src/common.h
+++ src/common.h
@@ -3,6 +3,7 @@
  */
 
 #pragma once
+
 #include <cstdint>
 
 namespace etsuko {
@@ -10,5 +11,5 @@ namespace etsuko {
         int32_t x, y, w, h;
     };
 
-    constexpr auto VERSION_STRING = "0.2.0";
+    constexpr auto VERSION_STRING = "0.2.5";
 }
