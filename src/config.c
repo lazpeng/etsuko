31a70afef1ba5c631466c83c30c3a51263e4ad43
Rafael Nakano <lazpeng@gmail.com>
2026-02-03 01:41:22 -0300

    Default config changes

--- src/config.c
+++ src/config.c
@@ -42,8 +42,8 @@ static Config_t *get_default_config(void) {
     }
     config->lyrics_font = strdup("NotoSans_ExtraCondensed-Bold.ttf");
     config->ui_font = strdup("NotoSans-Regular.ttf");
-    config->song_file = strdup("yoake.txt");
-    config->hide_past_lyrics = true;
+    config->song_file = strdup("tiny stars.txt");
+    config->hide_past_lyrics = false;
     config->draw_album_art_shadow = true;
     config->draw_lyric_shadow = true;
     config->show_loading_screen = true;
