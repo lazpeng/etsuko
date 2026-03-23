06843185a1a82e9671be0a650027c2d96079807c
Rafael Nakano <lazpeng@gmail.com>
2026-03-23 03:02:24 -0300

    bump version

--- src/constants.h
+++ src/constants.h
@@ -8,7 +8,7 @@
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
-#define VERSION "0.6.5b"
+#define VERSION "0.6.5"
 #define APP_NAME "etsuko"
 #define DEFAULT_TITLE APP_NAME " - Karaoke v" VERSION
 
